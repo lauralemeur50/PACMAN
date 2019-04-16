949a7742a55cb2651b1b980395f1398aa0c35c4e
lauralemeur50 <48725674+lauralemeur50@users.noreply.github.com>
2019-04-16 15:47:37 +0200

    Create constantes.py

---
+++ constantes.py
@@ -0,0 +1,21 @@
+"""Constantes du jeu pacman"""
+
+#Paramètres de la fenêtre
+nombre_sprite_cote = 17
+taille_sprite = 30
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+#Personnalisation de la fenêtre
+titre_fenetre = "Pacman Game"
+image_icone = "rond.png"
+
+#Listes des images du jeu
+image_accueil = "fond.jpeg"
+image_fond = "fond.jpeg"
+image_mur = "brique.png"
+image_depart = "depart.png"
+droite="pacman_droite.png"
+gauche="pacman_gauche.png"
+haut="pacman_haut.png"
+bas="pacman_bas.png"
+rond="rond.png"
