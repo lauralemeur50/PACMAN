fffc2d0bda9d2a24b33281462af2e10ab1a1a9a2
lauralemeur50 <48725674+lauralemeur50@users.noreply.github.com>
2019-04-23 15:06:03 +0200

    update

--- constantes.py
+++ constantes.py
@@ -1,21 +1,29 @@
 """Constantes du jeu pacman"""
 
 #Paramètres de la fenêtre
-nombre_sprite_cote = 170
-taille_sprite = 3
-cote_fenetre = nombre_sprite_cote * taille_sprite
+nombre_sprite_largeur = 30
+nombre_sprite_longueur=31
+taille_sprite = 12
+cote_largeur =  nombre_sprite_largeur* taille_sprite
+cote_longueur =  nombre_sprite_longueur* taille_sprite
 
 #Personnalisation de la fenêtre
 titre_fenetre = "Pacman Game"
-image_icone = "rond.png"
+image_icone = "pacman_d.png"
 
 #Listes des images du jeu
-image_accueil = "fond.jpeg"
-image_fond = "fond.jpeg"
-image_mur = "brique.png"
-image_depart = "depart.png"
-droite="pacman_droite.png"
-gauche="pacman_gauche.png"
-haut="pacman_haut.png"
-bas="pacman_bas.png"
-rond="rond.png"
+image_accueil = "background.jpg"
+image_fond = "background.jpg"
+image_ligne = "ligne_h.png"
+image_colonne="ligne_v.png"
+image_coin_1="coin_hd.png"
+image_coin_2="coin_bd.png"
+image_coin_3="coin_bg.png"
+image_coin_4="coin_hg.png"
+image_point="point.png"
+image_vide="vide.png"
+droite="pacman_d.png"
+gauche="pacman_g.png"
+haut="pacman_h.png"
+bas="pacman_b.png"
+rond="rond.jpg"
